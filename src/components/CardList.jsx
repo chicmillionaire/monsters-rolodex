723489dff0ee2b6f16d247f3647f4c8196353e97
chicmillionaire <chicmillionaire@gmail.com>
2020-09-02 21:04:31 -0400

    Initial commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card';
+
+import '../styles/CardList.css';
+
+const CardList = props => {
+  return (
+    <div className='card-list'>
+      {
+        props.monsters.map(monster => (
+          <Card key={monster.id} monster={monster}/> 
+       ))
+       }
+    </div>
+  );
+}
+
+export default CardList;
